ebffe3919c268fdc43ad60028ef0a44d26e91849
stevenbuccini <steven@stevenbuccini.com>
2013-03-07 20:48:41 -0800

    first commit

--- application.js
+++ application.js
@@ -1,73 +1,44 @@
 var nextImages = ""; //empty global variable that onReady and pagination functions can access
 
-$(document).ready(function() {
 //run function when window opens, loads the first 20 images.
 
+$(document).ready(function() {
     //Start with the most recent photo
     var currentindex = 0;
-
     //Load 20 photos at a time.
-    var maxindex = 20;
-
+    var maxIndex = 20;
     //the hashtag you wish to load from on Instagram
-    var tagname = 'holaindio';
-
-    while (currentindex < maxindex)
-    {
+    var tagName = 'YOUR TAG HERE';
+    //Access token needed for 
+    var apiToken = "YOUR ACCESS TOKEN HERE";
+    while (currentindex < maxindex){
         //grab JSON data from Instagram API
-        $.getJSON("http://198.101.229.108/get_screen_photos?printer_id=5&callback=?", function(instagramJSON){
-
-        //console.log(instagramJSON);
-        //console.log(instgramJSON.object[0]);
-
-        //Loop through each JSON entry
-        $(instagramJSON.photos).each(function(index, temp){
-                //console.log(temp.url);
-
-                //Add the image HTML to the Image div.
+        $.getJSON("https://api.instagram.com/v1/tags/" + tagName +"/media/recent?access_token=" + apiToken +"&callback=?", function(instagramJSON){
+            //Loop through each JSON entry
+            $(instagramJSON.photos).each(function(index, temp){
+                //Add each image HTML to the Image div.
                 $('#images').append('<img class="instagram" src="'+temp.url+'"></img>');
-                //$('.tagname').text(tagname);
-
-            });
-
-    }
-                //load more images via pagination function, pass in the pagination URL
-                nextImages = String(instagramJSON.pagination.next_url);
-                //console.log(instagramJSON.pagination.next_url);
-                //console.log(nextImages);
+            });}
+            //load more images via pagination function, pass in the pagination URL
+            nextImages = String(instagramJSON.pagination.next_url);  
         });
-
-
-
-
-
 });
 
 
-
-
+/** The pagination method grabs the next oldest 20 photos for a particular tag, then  adds those images to the correct div.
+    @param {String} url The pagination url given to us by the Instagram API, which will load the next number of pages automatically.
+*/ 
 function pagination(url) {
-//takes in the pagination URL from the JSON response from the Instagram API
-
     //increment the indicies to load next twenty photos.
     currentindex += 20;
     maxindex += 40
-
     //Load the new JSON
     $.getJSON(temp, function(instagramJSON){
-        //console.log(instagramJSON);
-        //console.log(instgramJSON.object[0]);
-
         //Loop through the JSON data
         $(instagramJSON.data).each(function(index, temp){
-                //console.log(temp);
-                //console.log(temp.images.standard_resolution.url);
-                $('#images').append('<img class="instagram" src="'+temp.url+'"></img>');
-
-            });
-
+            $('#images').append('<img class="instagram" src="'+temp.url+'"></img>');
+        });
             //reset the pagination script
-            nextImages = String(instagramJSON.pagination.next_url);
-
+        nextImages = String(instagramJSON.pagination.next_url);
     });
 }
